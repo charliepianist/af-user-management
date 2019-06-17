4e384311e5f77f645113b3763815133bd8adaf2a
will.schick <schicwp@gmail.com>
2019-06-17 09:41:27 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # java-starter
 
+
+## To build the app
+
+Type this command into the terminal
+
+    ./gradlew build
+    
+    
+## To run the app
+
+This command:
+
+
+     ./gradlew run
+
--- src/main/java/com/mni/Application.java
+++ src/main/java/com/mni/Application.java
@@ -6,6 +6,6 @@ package com.mni;
 public class Application {
 
     public static void main(String[] args){
-        System.out.println("Hello world");
+        System.out.println("\n\n------> Hello world!");
     }
 }
