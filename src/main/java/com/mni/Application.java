d0a521af685eed0b556d9f283a0c8639d9cd0858
will.schick <schicwp@gmail.com>
2019-06-17 09:36:58 -0600

    initial commit

---
+++ src/main/java/com/mni/Application.java
@@ -0,0 +1,11 @@
+package com.mni;
+
+/**
+ * Created by will.schick on 6/17/19.
+ */
+public class Application {
+
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
