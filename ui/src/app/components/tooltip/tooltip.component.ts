d7a3834fb1bb76c01980f4447f7660bd6994c6a5
Charles Liu <charles.liu@contractdev01.chi0.mni-news.com>
2019-08-12 12:03:54 -0500

    Nicer tooltips initial commit

--- ui/src/app/components/tooltip/tooltip.component.ts
+++ ui/src/app/components/tooltip/tooltip.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { isNullOrUndefined } from 'util';
 
 @Component({
   selector: 'app-tooltip',
@@ -16,9 +17,47 @@ export class TooltipComponent implements OnInit {
   @Input() size: number = 100;
   @Input() icon: boolean = true;
 
+  descriptionWidth: number = 160;
+
+  @ViewChild('description') descriptionElement: ElementRef;
+  canvas: HTMLCanvasElement;
+  ctx: CanvasRenderingContext2D;
+
   constructor() { }
 
   ngOnInit() {
+    if(!this.hasTextAndTooltip()) {
+      console.log('text or tooltip is null in TooltipComponent ' + this);
+    }
+  }
+
+  ngAfterViewInit() {
+    if(this.hasTextAndTooltip()) {
+      setTimeout(() => {
+          this.descriptionWidth = 
+            this.getUnwrappedTextWidthInPixels(
+              this.tooltip, 
+              this.getComputedProperty(this.descriptionElement, 'font')
+            ) * 2 / 3;
+        }
+      );
+    }
+  }
+
+  hasTextAndTooltip(): boolean {
+    return !(isNullOrUndefined(this.text) || isNullOrUndefined(this.tooltip));
+  }
+
+  getComputedProperty(element: ElementRef, property: string) {
+    return window.getComputedStyle(element.nativeElement).getPropertyValue(property);
+  }
+  getUnwrappedTextWidthInPixels(text: string, font: string): number {
+    if(isNullOrUndefined(this.canvas)){
+        this.canvas = document.createElement('canvas');
+        this.ctx = this.canvas.getContext('2d');
+    }
+    this.ctx.font = font;
+    return this.ctx.measureText(text).width;
   }
 
 }
